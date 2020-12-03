ddee467052bb67ae474e33437a82f80f208556ea
Andres <andres_masis_rojas@outlook.com>
2020-12-03 15:25:12 -0600

    The KeyListener is just in one class, still habe to figure out how to hide the GUI.

--- Simulator/src/simulator/Controller.java
+++ Simulator/src/simulator/Controller.java
@@ -1,55 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package simulator;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.JFrame;
 
 
-public class Controller implements KeyListener{
-    private int option;
-     
-    // Detects if any key is presses
-    @Override
-    public void keyPressed(KeyEvent e) {
-        int code = e.getKeyCode();
-        
-        if (code == KeyEvent.VK_UP){
-            // The up arrow has been pressed
-            System.out.println("1");
+public class Controller extends JFrame {
+
+    public Controller() {
+        JFrame frame = new JFrame();
+        frame.setVisible(false);
+        frame.setSize(1, 1);
+        frame.setFocusable(true);
+
+        frame.addKeyListener(new KeyListener() {
+
             
-        } else if (code == KeyEvent.VK_DOWN){
-            // The down arrow has been pressed
-            System.out.println("2");
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int code = e.getKeyCode();
+
+                if (code == KeyEvent.VK_UP) {
+                    System.out.println("Hola");
+                    // The up arrow has been pressed
+
+                } else if (code == KeyEvent.VK_DOWN) {
+                    // The down arrow has been pressed;
+
+                } else if (code == KeyEvent.VK_LEFT) {
+                    // The left arrow has been pressed
+
+                } else if (code == KeyEvent.VK_RIGHT) {
+                    // The right arrow has been pressed
+
+                } else if (code == KeyEvent.VK_SPACE) {
+                    System.out.println("ADIOS");
+                    // The spacebar has been pressed
+                }
+            }
             
-        } else if (code == KeyEvent.VK_LEFT){
-            // The left arrow has been pressed
-            System.out.println("3");
-            
-        } else if (code == KeyEvent.VK_RIGHT){
-            // The right arrow has been pressed
-            System.out.println("4");
-            
-        } else if (code == KeyEvent.VK_SPACE){
-            // The spacebar has been pressed
-            System.out.println("5");
-        }
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // Not used
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // Not used    
+            }
+
+        });
     }
-   
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // Not used
+    public static void main(String args[]) {
+        new Controller();
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // Not used
-    }
-
-   
-    
 }
--- Simulator/src/simulator/Simulator.java
+++ Simulator/src/simulator/Simulator.java
@@ -1,3 +1,4 @@
+  
 /*
  * This is the part that controls the logic of the console
  */
@@ -5,18 +6,19 @@ package simulator;
 
 import javax.swing.JFrame;
 
+
+
 public class Simulator extends JFrame{
 
-    public Simulator(){
+/*    public Simulator(){
         this.setLayout(null);
         Controller input = new Controller();
         addKeyListener(input);
     }
     
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Simulator sim = new Simulator();
         sim.setVisible(true);
         sim.setBounds(0,0,1,1);
-    }
-    
+    }*/
 }
